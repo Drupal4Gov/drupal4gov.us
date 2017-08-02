b15454de285b249570ac34f8282e59a9373b988a
Mike Madison <mike.madison@acquia.com>
2017-08-02 12:59:53 -0400

    D4G-17: adding Drupal VM to repo.

--- drush/site-aliases/aliases.drushrc.php
+++ drush/site-aliases/aliases.drushrc.php
@@ -359,3 +359,18 @@
 #    'uri' => 'http://example.com',
 #    'root' => $aliases['dev']['root'],
 #  ) + $aliases['server'];
+
+// Local environment.
+$aliases['drupal4gov.local'] = array(
+  'root' => '/var/www/drupal4gov/docroot',
+  'uri' => 'http://local.drupal4gov.com',
+  );
+// Add remote connection options when alias is used outside VM.
+if ('vagrant' != $_SERVER['USER']) {
+  $aliases['drupal4gov.local'] += array(
+    'remote-host' => 'local.drupal4gov.com',
+    'remote-user' => 'vagrant',
+    'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
+  );
+}
+
