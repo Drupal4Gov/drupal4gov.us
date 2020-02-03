069f91ddf276df5da8bf087cbd0bf51b1fa67e9b
Mike Madison <mike.madison@acquia.com>
2020-02-03 09:28:58 -0800

    DG4-000: project template updates.

--- README.md
+++ README.md
@@ -2,85 +2,79 @@ Drupal4Gov website
 
 ## Getting Started
 
-This project is based on BLT, an open-source project template and tool that enables building, testing, and deploying Drupal installations following Acquia Professional Services best practices.
+This project built with BLT, an open-source project template and tool that enables building, testing, and deploying Drupal installations following Acquia Professional Services best practices.
 
-* Ensure that your computer meets the minimum installation requirements (and then install the required applications). See the [System Requirements](http://blt.readthedocs.io/en/8.x/INSTALL/).
+* Ensure that your computer meets the minimum installation requirements (and then install the required applications). See the [BLT System Requirements](https://docs.acquia.com/blt/install/) and [Lando System Requirements](https://docs.lando.dev/basics/installation.html).
 * Fork the parent repository in GitHub
 * Request access to the Drupal4Gov organization in GitHub 
-* Request access to the Acquia Cloud Environment for Drupal4Gov
+* Request access to the Acquia Cloud Environment for Drupal GovCon
 * Setup a SSH key that can be used for GitHub and the Acquia Cloud (you CAN use the same key)
     * [Setup GitHub SSH Keys](https://help.github.com/articles/adding-a-new-ssh-key-to-your-github-account/)
     * [Setup Acquia Cloud SSH Keys](https://docs.acquia.com/acquia-cloud/ssh/generate)
-* Clone your fork
-```
-$ git clone git@github.com:<your repository>/drupal4gov.us.git
-```
-* Add the parent repository as an upstream
-```
-$ git remote add upstream git@github.com:Drupal4Gov/drupal4gov.us.git
-```
-* Install Composer Dependencies (warning: this can take some time based on internet speeds)
-```
-$ composer install
-```
-* Setup Virtual Machine (warning: this can take some time based on internet speeds)
-```
-$ blt vm
-```
-* Syncronize your local with the cloud
-```
-$ blt sync:refresh
-``` 
-* Access the site and do necessary work at http://local.drupal4gov.com
 
-Additional [BLT documentation](http://blt.readthedocs.io) may be useful. You may also access a list of BLT commands by running:
-```
-$ blt
-``` 
+After creating a fork of the repository in Github 9and installing all dependencies above) there are only 4 commands to get started:
 
-Note the following properties of this project:
-* Primary development branch: master
-* Local environment: DrupalVM
-* Local drush alias: @drupal4gov.local
-* Local site URL: http://local.drupal4gov.com
+* `git clone` (clone your fork)
+* `composer install` (install dependencies for the project)
+* `lando start` (provision the vm)
+* `lando blt setup` (install Drupal)
 
-## Working With BLT
+## Updating from DrupalVM
 
-This is the common workflow for this project.
+If you have been a part of our team in the past and have a local DrupalVM, here are a few steps to take to "update" to Lando.
 
-* Locate a ticket that you are planning on working
-* Ensure that your git is tracking the most current upstream.
+* `blt vm:nuke` to destroy the old vm
+* open the blt/local.blt.yml file and look for a "vm" key. If present, remove the vm (and enable:true) keys from the file
+* open the docroot/sites/default/settings/local.settings.php file and replace the database configuration with:
+
+```php
+/**
+ * Database configuration.
+ */
+$databases = array(
+  'default' =>
+  array(
+    'default' =>
+    array(
+      'database' => 'drupal8',
+      'username' => 'drupal8',
+      'password' => 'drupal8',
+      'host' => 'database',
+      'port' => '3306',
+      'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 ```
-$ git fetch upstream
-```
-* Create a new branch off of upstream/master that is based on the ticket you are working (e.g. D4G-XXX)
-```
-$ git checkout -b D4G-XXX upstream/master
-```
-* Reset local environment to ensure all is inline with new branch. WARNING: this is destructive
-```
-$ blt sync:refresh
-```
-* Do whatever work is required for ticket
-* Create new commit(s) as needed. All commit messages should follow the pattern: D4G-XXX: commit messages go here. They must include the Ticket Number (with a dash AND a colon), a message, and a period.
-* Run Tests / Validation Scripts
-```
-$ blt validate
-$ blt tests
-```
-* Ensure no other changes have been made to the upstream/master branch. If they have, rebase your branch.
-```
-$ git fetch upstream
-$ git rebase upstream/master
-```
-* Push your commit(s) to your origin
-```
-$  git push --set-upstream origin D4G-XXX
-```
-* Create a new Pull Request that mentions the original ticket in the body (#DGC-XXX)
-* Ensure the build passes
+
+* run `lando start`
+* run `lando blt setup`
+
+Note that the new local project url is http://drupalgovcon.lndo.site:8080
+
+## Working with Lando and BLT
+
+Our team utilizes a standard [Git flow](https://www.atlassian.com/git/tutorials/comparing-workflows/gitflow-workflow) for our development workflow. You can read more about our recommended workflow in the [BLT docs](https://docs.acquia.com/blt/developer/dev-workflow/#workflow-example-local-development).
+
+In general, "all" commands (drush, blt, etc.) should be run "inside" the Lando container. You can do this by first running the `lando` command. 
+
+For example:
+
+Cleanly Install Drupal:
+
+`lando blt setup` 
+
+Sync Drupal Database from Cloud:*
+
+`lando blt sync`
+
+Log Into Drupal
+
+`lando drush uli`
 
 ## Resources
 
 * JIRA - https://drupal4gov.atlassian.net
-* GitHub - https://github.com/Drupal4Gov/drupal4gov.us
+* GitHub - https://github.com/Drupal4Gov/Drupal-GovCon-2017
--- tests/behat/features/bootstrap/Drupal/FeatureContext.php
+++ tests/behat/features/bootstrap/Drupal/FeatureContext.php
@@ -4,6 +4,12 @@ namespace Drupal;
 
 use Drupal\DrupalExtension\Context\RawDrupalContext;
 use Behat\Behat\Context\SnippetAcceptingContext;
+use PHPUnit\Framework\Assert;
+use Drupal\user\Entity\Role;
+use Behat\Gherkin\Node\TableNode;
+use Drupal\node\Entity\NodeType;
+use Drupal\media\Entity\MediaType;
+use Drupal\block_content\Entity\BlockContentType;
 
 /**
  * FeatureContext class defines custom step definitions for Behat.
@@ -18,6 +24,398 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
    */
   public function __construct() {
 
+  }
+
+  /**
+   * Entity Functions.
+   */
+
+  /**
+   * @Given the :arg1 content type exists
+   *
+   * Examples:
+   * Given the "blog" content type exists
+   *
+   * @throws \Exception;
+   */
+  public function contentTypeExists($string) {
+    $node_type = NodeType::load($string);
+    if (empty($node_type)) {
+      throw new \Exception('Content type ' . $string . ' does not exist.');
+    }
+  }
+
+  /**
+   * @Given the :arg1 media type exists
+   *
+   * Examples:
+   * Given the "image" media type exists
+   *
+   * @throws \Exception;
+   */
+  public function mediaTypeExists($string) {
+    $media_type = MediaType::load($string);
+    if (empty($media_type)) {
+      throw new \Exception('Media type ' . $string . ' does not exist.');
+    }
+  }
+
+  /**
+   * @Given the :arg1 block_content type exists
+   *
+   * Examples:
+   * Given the "hero" block_content type exists
+   *
+   * @throws \Exception;
+   */
+  public function blockTypeExists($string) {
+    $block_type =  BlockContentType::load($string);
+    if (empty($block_type)) {
+      throw new \Exception('Block Content type ' . $string . ' does not exist.');
+    }
+  }
+
+  /**
+   * Check for presence of a field on a bundle.
+   *
+   * Examples:
+   * Then the field "body" is present on the "blog" "node" type
+   * Then the field "body" is present on the "hero" "block_content" type
+   * Then the field "body" is present on the "slide" "paragraph" type
+   * Then the field "body" is present on the "image" "media" type
+   * Then the field "body" is present on the "tag" "vocabulary" type
+   *
+   * @Then the field :arg1 is present on the :arg2 :arg3 type
+   */
+  public function isField($field_name, $bundle, $entity) {
+    $bundle_fields = \Drupal::getContainer()->get('entity_field.manager')->getFieldDefinitions($entity, $bundle);
+    if (empty($bundle_fields[$field_name])) {
+      Assert::assertEmpty($bundle_fields, $field_name . ' is not present on the ' . $entity . " " . $bundle);
+    }
+  }
+
+  /**
+   * Check if a present field is required on a bundle.
+   *
+   * Examples:
+   * Then the field "body" is required on the "blog" "node" type
+   * Then the field "body" is required on the "hero" "block_content" type
+   * Then the field "body" is required on the "slide" "paragraph" type
+   * Then the field "body" is required on the "image" "media" type
+   * Then the field "body" is required on the "tag" "vocabulary" type
+   *
+   * @Then the field :arg1 is required on the :arg2 :arg3 type
+   */
+  public function isRequiredField($field_name, $bundle, $entity) {
+    $bundle_fields = \Drupal::getContainer()->get('entity_field.manager')->getFieldDefinitions($entity, $bundle);
+    $field_definition = $bundle_fields[$field_name];
+    $setting = $field_definition->isRequired();
+    Assert::assertNotEmpty($setting, 'Field ' . $field_name . ' is not required.');
+  }
+
+  /**
+   * Check if a present field is not required on a bundle.
+   *
+   * Examples:
+   * Then the field "body" is not required on the "blog" "node" type
+   * Then the field "body" is not required on the "hero" "block_content" type
+   * Then the field "body" is not required on the "slide" "paragraph" type
+   * Then the field "body" is not required on the "image" "media" type
+   * Then the field "body" is not required on the "tag" "vocabulary" type
+   *
+   * @Then the field :arg1 is not required on the :arg2 :arg3 type
+   */
+  public function isNotRequiredField($field_name, $bundle, $entity) {
+    $bundle_fields = \Drupal::getContainer()->get('entity_field.manager')->getFieldDefinitions($entity, $bundle);
+    $field_definition = $bundle_fields[$field_name];
+    $setting = $field_definition->isRequired();
+    Assert::assertNotEmpty($setting, 'Field ' . $field_name . ' is not required.');
+  }
+
+  /**
+   * Check a reference field's target bundle.
+   *
+   * Examples:
+   * Then the field "categories" on the "blog" "node" type allows references to "categories"
+   * Then the field "categories" on the "hero" "block_content" type allows references to "categories"
+   * Then the field "categories" on the "slide" "paragraph" type allows references to "categories"
+   * Then the field "categories" on the "image" "media" type allows references to "categories"
+   * Then the field "categories" on the "tag" "vocabulary" type allows references to "categories"
+   *
+   * @Then the field :arg1 on the :arg2 :arg3 type should allow references to :arg4
+   */
+  public function fieldAllowsEntityReferences($field_name, $bundle, $entity, $reference_bundle) {
+    $bundle_fields = \Drupal::getContainer()->get('entity_field.manager')->getFieldDefinitions($entity, $bundle);
+    $field_definition = $bundle_fields[$field_name];
+    $settings = $field_definition->getSettings();
+    if (!empty($settings['handler_settings']['target_bundles'])) {
+      $target_bundles = $settings['handler_settings']['target_bundles'];
+    }
+    elseif (!empty($settings['handler_settings']['target_bundles_drag_drop'])) {
+      $target_bundles = $settings['handler_settings']['target_bundles_drag_drop'];
+      foreach ($target_bundles as $key=>$bundle) {
+        $target_bundles[$key] = $key;
+      }
+    }
+    else {
+      return false;
+    }
+    Assert::assertContains(trim($reference_bundle), $target_bundles, $field_name . ' does not allow references to ' . trim($reference_bundle) . ' content');
+  }
+
+  /**
+   * Check if a particular field is visible on a particular entity display mode
+   *
+   * Examples:
+   * Then the display "teaser" on the "blog" "node" type should display the "field_status" field
+   * Then the display "teaser" on the "hero" "block_content" type should display the "field_status" field
+   * Then the display "teaser" on the "slide" "paragraph" type should display the "field_status" field
+   * Then the display "teaser" on the "image" "media" type should display the "field_status" field
+   * Then the display "teaser" on the "tag" "vocabulary" type should display the "field_status" field
+   *
+   * @Then the display :arg1 on the :arg2 :arg3 type should display the :arg4 field
+   *
+   */
+  public function nodeFieldVisibile($display, $bundle, $entity, $field_name) {
+    $storage = \Drupal::entityTypeManager()->getStorage('entity_view_display');
+    $view_display = $storage->load("$entity.$bundle.$display");
+    $component = $view_display->getComponent($field_name);
+    Assert::assertContains('content', $component['region']);
+  }
+
+  /**
+   * Check if a particular field is not visible on a particular entity display mode
+   *
+   * Examples:
+   * Then the display "teaser" on the "blog" "node" type should not display the "field_status" field
+   * Then the display "teaser" on the "hero" "block_content" type should not display the "field_status" field
+   * Then the display "teaser" on the "slide" "paragraph" type should not display the "field_status" field
+   * Then the display "teaser" on the "image" "media" type should not display the "field_status" field
+   * Then the display "teaser" on the "tag" "vocabulary" type should not display the "field_status" field
+   *
+   * @Then the display :arg1 on the :arg2 :arg3 type should not display the :arg4 field
+   */
+  public function nodeFieldNotVisibile($display, $bundle, $entity, $field_name) {
+    $storage = \Drupal::entityTypeManager()->getStorage('entity_view_display');
+    $view_display = $storage->load("$entity.$bundle.$display");
+    $component = $view_display->get('hidden');
+    Assert::assertEquals(true, $component[$field_name]);
+  }
+
+  /**
+   *
+   * Checks the cardinality of a field on a given bundle.
+   *
+   * Examples:
+   * The field "categories" on the "node" bundle has a cardinality of "-1".
+   * The field "categories" on the "block_content" bundle has a cardinality of "-1".
+   * The field "categories" on the "paragraph" bundle has a cardinality of "-1".
+   * The field "categories" on the "media" bundle has a cardinality of "-1".
+   * The field "categories" on the "vocabulary" bundle has a cardinality of "-1".
+   *
+   * @Then the field :arg1 on the :arg2 bundle has a cardinality of :arg3
+   *
+   */
+  public function checkCardinality($field, $bundle, $cardinality) {
+    $config = $this->checkCardinality($bundle, $field, $cardinality);
+    Assert::assertEquals(
+      $cardinality,
+      $config,
+      "The $field does not have the correct cardinality. It should be $cardinality but in reality is $config."
+    );
+  }
+
+  /**
+   * Checks the maximum length of a field on a given bundle.
+   *
+   * Examples:
+   * The field "title" on the "node" bundle has a maximum length of "100"
+   * The field "title" on the "block_content" bundle has a maximum length of "100"
+   * The field "title" on the "paragraph" bundle has a maximum length of "100"
+   * The field "title" on the "media" bundle has a maximum length of "100"
+   * The field "title" on the "vocabulary" bundle has a maximum length of "100"
+   *
+   * @Then the field :arg1 on the :arg2 :arg3 type has a maximum length of :arg4
+   */
+  public function nodeFieldLengthLimit($field_name, $bundle, $entity, $length) {
+    $bundle_fields = \Drupal::getContainer()->get('entity_field.manager')->getFieldDefinitions($entity, $bundle);
+    $field_definition = $bundle_fields[$field_name];
+    $settings = $field_definition->getSettings();
+    Assert::assertEquals($length, $settings['max_length']);
+  }
+
+  /**
+   * @Then I visit the last created :arg1
+   *
+   * Locate the last created node or media and then redirect to it.
+   *
+   * Examples:
+   * Then I visit the last created node.
+   * Then I visit the last created media.
+   */
+  public function iVisitLatestContent($bundle) {
+    switch ($bundle) {
+      case "node":
+      default:
+        $id = $this->getLastCreatedNode();
+        break;
+      case "media":
+        $id = $this->getLastCreatedMedia();
+        break;
+    }
+    $this->getSession()->visit($this->locatePath("$bundle/$id"));
+
+  }
+
+  /**
+   * Permission Functions.
+   */
+
+  /**
+   * @Given the following roles have these permissions:
+   *
+   * Examples:
+   * Given the following roles have these permissions:
+   * | role                  | permission                                  |
+   * | anonymous             | access content                              |
+   * | authenticated         | access content                              |
+   * | reviewer              | use content_workflow transition ready_ready |
+   */
+  public function roleUserPermissions(TableNode $rolesTable) {
+    foreach ($rolesTable as $rolePermission) {
+      $role = $rolePermission['role'];
+      $permission = $rolePermission['permission'];
+      $this->roleHasPermission($role, $permission);
+    }
+  }
+
+  /**
+   * @Given the following roles do not have these permissions:
+   *
+   * Examples:
+   * Given the following roles do not have these permissions:
+   * | role                  | permission                                  |
+   * | anonymous             | access content                              |
+   * | authenticated         | access content                              |
+   * | reviewer              | use content_workflow transition ready_ready |
+   */
+  public function roleUserPermissionsNot(TableNode $rolesTable) {
+    foreach ($rolesTable as $rolePermission) {
+      $role = $rolePermission['role'];
+      $permission = $rolePermission['permission'];
+      $this->roleDoesNotHavePermission($role, $permission);
+    }
+  }
+
+  /**
+   * @Then :arg1 role has permission to :arg2
+   *
+   * Examples:
+   * Then the "reviewer" role has permission to "access content"
+   */
+  public function checkRolePermissions($role, $permission) {
+    $this->roleHasPermission($role, $permission);
+  }
+
+  /**
+   * @Then :arg1 role does not have permission to :arg2
+   *
+   * Examples:
+   * Then the "reviewer" role does not have permission to "access content"
+   */
+  public function checkRoleDoesNotHavePermission($role, $permission) {
+    $this->roleDoesNotHavePermission($role, $permission);
+  }
+
+  /**
+   * Checks that only valid roles have permission to execute certain content actions
+   *
+   * Examples:
+   * | role                    | permission |
+   * | author           	     | create     |
+   * | author           	     | edit own   |
+   * | editor           	     | edit any   |
+   * | content_administrator   | create     |
+   * | content_administrator	 | edit own   |
+   * | content_administrator	 | edit any   |
+   *
+   * @Given that only the following roles have content permissions for the :arg1 content type:
+   *
+   */
+  public function roleOnlyContentPermissions($bundle, TableNode $rolesTable) {
+    $allowed_roles = array();
+    foreach ($rolesTable as $rolePermission) {
+      $role = $rolePermission['role'];
+      $permission = $rolePermission['permission'] . ' ' . $bundle . ' content';
+      $this->roleHasPermission($role, $permission);
+      $allowed_roles[] = $role;
+    }
+    $allowed_roles[] = 'administrator';
+
+    $all_roles = $this->getRoles();
+    foreach ($all_roles as $role) {
+      if (!in_array($role, $allowed_roles)) {
+        $this->roleDoesNotHavePermission($role, $permission);
+      }
+    }
+  }
+
+  /**
+   * Helper Functions Only below.
+   */
+
+
+  /**
+   * Get most recent node id.
+   *
+   */
+  public static function getLastCreatedNode() {
+    $query = \Drupal::database()->select('node_field_data', 'nfd');
+    $query->addField('nfd', 'nid');
+    $query->range(0, 1);
+    $query->orderBy("nid", 'DESC');
+    $nid = $query->execute()->fetchField();
+
+    return $nid;
+  }
+
+  /**
+   * Get most recent media id.
+   *
+   */
+  public static function getLastCreatedMedia() {
+    $query = \Drupal::database()->select('media_field_data', 'mfd');
+    $query->addField('mfd', 'mid');
+    $query->range(0, 1);
+    $query->orderBy("mid", 'DESC');
+    $mid = $query->execute()->fetchField();
+
+    return $mid;
+  }
+
+  /**
+   * Users with the $role should have the $permission.
+   */
+  public function roleHasPermission($role, $permission) {
+    $roleObj = Role::load($role);
+    Assert::assertNotEmpty($roleObj->hasPermission($permission), $role . ' role does not have permission to ' . $permission);
+  }
+
+  /**
+   * Users with the $role should not have the $permission.
+   */
+  public function roleDoesNotHavePermission($role, $permission) {
+    $roleObj = Role::load($role);
+    Assert::assertEmpty($roleObj->hasPermission($permission), $role . ' role has permission to ' . $permission . ', but should not.');
+  }
+
+  /**
+   * Get all user roles.
+   */
+  public function getRoles() {
+    $roles = user_roles();
+    $roles = array_keys($roles);
+    return $roles;
   }
 
 }
